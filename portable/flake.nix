ac71ea6ec9a64f2ddb109bf15528ea5237ca8ae4
anthony-j-g <anthonyjbasai@gmail.com>
2025-06-29 12:42:51 -0400

    [modules] adding more placeholder files

--- portable/flake.nix
+++ portable/flake.nix
@@ -15,7 +15,7 @@
     ...
   }: {
     nixosConfigurations = {
-      #
+      # Baseline server deployment
       foxcatcher = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs =
@@ -39,35 +39,35 @@
         ];
       };
 
-      shoukei = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs =
-          inputs
-          // {
-            myvars.username = "ajg";
-            myvars.userfullname = "Anthony J. G.";
-          };
-        modules = [
-          # Building on a USB installer is buggy, lack of disk space, memory, trublesome to setup substituteers, etc.
-          # so we disable apple-t2 module here to avoid build kernel during the initial installation, and enable it after the first boot.
-          # nixos-hardware.nixosModules.apple-t2
-          ({pkgs, ...}: {
-            networking.hostName = "shoukei";
-            boot.kernelPackages = pkgs.linuxPackages_latest; # Use latest kernel for the initial installation.
-            # hardware.apple-t2.enableAppleSetOsLoader = true;
-          })
-
-          ./configuration.nix
-
-          ../modules/base
-          ../modules/nixos/base/i18n.nix
-          ../modules/nixos/base/user-group.nix
-          ../modules/nixos/base/networking.nix
-
-          ../hosts/12kingdoms-shoukei/hardware-configuration.nix
-          ../hosts/idols-ai/preservation.nix
-        ];
-      };
+      # shoukei = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   specialArgs =
+      #     inputs
+      #     // {
+      #       myvars.username = "ajg";
+      #       myvars.userfullname = "Anthony J. G.";
+      #     };
+      #   modules = [
+      #     # Building on a USB installer is buggy, lack of disk space, memory, trublesome to setup substituteers, etc.
+      #     # so we disable apple-t2 module here to avoid build kernel during the initial installation, and enable it after the first boot.
+      #     # nixos-hardware.nixosModules.apple-t2
+      #     ({pkgs, ...}: {
+      #       networking.hostName = "shoukei";
+      #       boot.kernelPackages = pkgs.linuxPackages_latest; # Use latest kernel for the initial installation.
+      #       # hardware.apple-t2.enableAppleSetOsLoader = true;
+      #     })
+      #
+      #     ./configuration.nix
+      #
+      #     ../modules/base
+      #     ../modules/nixos/base/i18n.nix
+      #     ../modules/nixos/base/user-group.nix
+      #     ../modules/nixos/base/networking.nix
+      #
+      #     ../hosts/12kingdoms-shoukei/hardware-configuration.nix
+      #     ../hosts/idols-ai/preservation.nix
+      #   ];
+      # };
     };
   };
 }
